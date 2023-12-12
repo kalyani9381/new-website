4a66a95747ce247ef66d640ed66c414484c1192e
kalyani@123 <sankranthikalyani1999@gmail.com>
2023-12-12 10:56:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./Component/MainFile/mainFile";
+
+function App() {
+  return (
+    <div className="">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
